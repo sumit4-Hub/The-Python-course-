27d5d4fd6dcbbccbf416f5409b4cb783c2f202c2
Code With Sumit <sumitchowdhuary4@gmail.com>
2025-05-03 20:46:20 +0530

    Add files via upload

---
+++ Chapter_1 (MODULES, COMMENTS & PIP)/module.py
@@ -0,0 +1,13 @@
+import pyjokes
+
+# print("printing Jokes")
+
+# This prints a random joke
+joke = pyjokes.get_joke()
+print(joke)
+
+
+# so thanks 
+# that was my program
+# another line
+# yet another line
