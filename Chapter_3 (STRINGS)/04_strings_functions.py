ee8156bb30d8ba1f99cb80250ec422fc90353d8e
Code With Sumit <sumitchowdhuary4@gmail.com>
2025-05-05 10:41:09 +0530

    Add files via upload

---
+++ Chapter_3 (STRINGS)/04_strings_functions.py
@@ -0,0 +1,9 @@
+name = "sumit"
+
+print(len(name))
+print(name.endswith("mit"))
+print(name.startswith("su"))
+print(name.capitalize())
+
+replace_string = name.replace("sumit", "prity")
+print(replace_string)
---
+++ Chapter_3 (STRINGS)/05_escape_sequence_characters.py
@@ -0,0 +1,8 @@
+a = "sumit is a good boy \nbut not a bad boy"
+print(a)
+
+b = "sumit is a good boy \nbut not a bad \"boy\""
+print(b)
+
+c = "sumit is a good boy \tbut not a bad \"boy\""
+print(c)
