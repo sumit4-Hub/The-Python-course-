4c025004321a528324fdca8bc5ac672babd7649f
Code With Sumit <sumitchowdhuary4@gmail.com>
2025-05-05 13:10:21 +0530

    Add files via upload

---
+++ Chapter_4 Practice set/problem2.py
@@ -0,0 +1,26 @@
+Marks = []
+
+f1 = int(input("Enter marks here: "))
+Marks.append(f1)
+
+f2 = int(input("Enter marks here: "))
+Marks.append(f2)
+
+f3 = int(input("Enter marks here: "))
+Marks.append(f3)
+
+f4 = int(input("Enter marks here: "))
+Marks.append(f4)
+
+f5 = int(input("Enter marks here: "))
+Marks.append(f5)
+
+f6 = int(input("Enter marks here: "))
+Marks.append(f6)
+
+Marks.sort()
+
+print(Marks)
+
+
+
---
+++ Chapter_4 Practice set/problem3.py
@@ -0,0 +1,5 @@
+a = (45, 87, "sumit")
+
+# Once a tuple is created, you cannot change, add, or remove its elements and also  tuple is immutable.
+a[2] = "puja"  
+
