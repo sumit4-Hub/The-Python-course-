f924a21de8f6f94b7c5b80629f9b9f135e512910
Code With Sumit <sumitchowdhuary4@gmail.com>
2025-05-11 22:42:53 +0530

    Add files via upload

---
+++ Chapter_5 (Practice set)/problem1.py
@@ -0,0 +1,11 @@
+words = {
+    "नमस्ते" :  " Hello",
+    "धन्यवाद" : " Thank you",
+    "कृपया" :  "Please",
+    "हाँ" :     "Yes",
+    "नहीं" :    "No",
+    "पानी" :    "Water"
+}
+
+word = input("Enter the word you want meaning of :")
+print(words[word])
---
+++ Chapter_5 (Practice set)/problem3.py
@@ -0,0 +1,5 @@
+s = set()
+
+s.add(18)   # these is the integer
+s.add("18")  # these is the string
+print(s)
